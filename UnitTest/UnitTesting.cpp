49ab42007555354a3b606e9a7eafa65847440193
Michael Conard <maconard@mtu.edu>
2017-10-17 13:38:17 -0400

    comments

--- UnitTest/UnitTesting.cpp
+++ UnitTest/UnitTesting.cpp
@@ -2,7 +2,8 @@
 
 #if DO_UNIT_TESTING==1
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+//Catch provides a main that takes precedence
+#define CATCH_CONFIG_MAIN
 
 #include <catch.hpp>
 
